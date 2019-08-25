31022c146c32dd99335704dafa68535e31e152f2
vtnovk <vtnovk@gmail.com>
2019-08-25 09:33:29 -0400

    created the whole f-ing thing

--- README.md
+++ README.md
@@ -1,4 +1,40 @@
-# Just An F-Ing Microsite Skeleton
+# Just A F-ing Minisite Skeleton
 or JAFMS for shrt.
 
-Soon, this will become the simplest, stupidest and most non-fancy way to make your self a simple f-ing microsite.
+The most simple responsive single page minisite or microsite template. Built on Twitter Bootstrap, all dependencies included, no compilation required. F-k it, it even works without PHP, if you don't need dynamic photo gallery handling. Pure static html + css + js website.
+
+Live example is currently available at [https://just-an-f-ing-microsite-skeleton-vtnovk529892.codeanyapp.com/](https://just-an-f-ing-microsite-skeleton-vtnovk529892.codeanyapp.com/).
+
+## What is it for?
+It's for when you need to make a f-ing nice looking mobile-first responsive single page micro/mini-site for your friend or whoever, but you do it like once or twice a year, so you don't have any tools for proper development up to date, so you ain't gonna be compiling lesses/sasses on boilerplates bowers gulps grunts f-ing shts.
+
+Or you are just too old (or simply can't be fucking bothered) to keep up with all that. (My case)
+
+It's a web page. No f-ing rocket science.
+
+## What do I do with it?
+You just grab it from here, copy it where the f-k ever you want it, delete everything you don't need, copy the sht you need as many times as you need, change some texts, pictures colors and sht and voila - you've got you self a f-ing simple single page website in a f-ing blink of an eye.
+
+## Well that sounds f-ing awesome! What do I need to make it work?
+Are you f-ing stupid or something? Do you read what I write? You need nothing. Everything is packed in this repo already.
+
+## So what's in the repo then?
+The whole f-ing thing is built on [Twitter Bootstrap](https://getbootstrap.com/). So a local copy is included. Also the photo gallery utilizes [Justified Gallery](https://miromannino.github.io/Justified-Gallery/) and [PhotoSwipe](https://photoswipe.com/), also included. And let's not forget [Font Awesome](https://fontawesome.com/). And [jQuery](https://jquery.com/). Shut up.
+
+## Why is it all in the repo and not included on compilation or something?
+Well because there is no f-ing compilation or something, is there!? I was tired of that sht. I just want to grab code, change a few strings and pictures and make it online. I don't do it on daily basis, I don't have my environment set up for this sht and I don't want to be doing that. Ain't no one got time for that sht.
+
+## Do I need to configure someting to make it run?
+I strongly suggest to use absolute URIs. Since this is very static thing, you would need to go through the code and manualy insert your base URI to where ever needed. Do that. And I'm talking specificaly about index.html and js/main.js.
+
+## Regarding PHP
+There IS the option of using some photo gallery handling on the server side. If you have PHP (I guess 5.6+ would do, but you're f-ed in your head if you don't have at least 7 already), you can go check out how gallery-server.php works. If not, and you're ok with hand-made static gallery, just cut that ajax sht off and hard-code it the old way.
+
+## Will there be more documentation/tutorials on how to make this work?
+Well, not by me, that's for sure. As far as I'm concerned, this matter is so simple, you can just look into the four source files (index.html, js/main.js, css/style.css and gallery-server.php) and if you're not completely brain-dead, you have to be able to find out wtf is going on there.
+
+## Can I contribute?
+Yes you can, but beware: NO F-ING FANCY STUFF! No f-ing compilation, deploy, automation, building, making, package management... NO! This thing needs to be kept as simple and as independent on environment (both dev and production) as f-ing possible.
+
+## I don't like --something-- about his project.
+Well, I most probably don't give a f-k. You can either put up with it, not use it, or **GO FORK YOUR SELF**.
---
+++ gallery-server.php
@@ -0,0 +1,286 @@
+<?php
+/**
+* This is a part of the "Just A F-ing Mini Skeleton" project.
+* GitHub: https://github.com/vinovak/just-a-f-ing-minisite-skeleton
+*
+* This particular script is the simplest and easiest way I could
+* thing of to handle very basic photogallery related operations.
+*
+* It generates thumbnails when missing, to have them physicaly
+* on the server in given directory for all the crop mode + supporter
+* size combinations.
+*
+* It does that on the fly, when http request commes. So when there
+* is a lot ofcombinations and/or a lot of pictures, it may time out.
+* This timeout should have no side effects, so you can keep calling
+* it untill all the thumbnails are pregenerated. Therefor, if you're
+* not able to set the time limit a bit longer, I suggest calling
+* this script separately by hand when ever you add new fullsize
+* pictures and/or supported crop modes and sizes.
+*
+* It might sound very lame, but right now it's all I need. It has
+* absolutely zero ambition to do more than it does right now. 
+* So if it does not suit your needs, you are going to have to 
+* improve it on you self, or just find another solution.
+*
+* Oh and it needs write permissions inside the gallery folder.
+*
+* Oh and Imagick for PHP available on server.
+*/
+
+/**
+* If you turn debug mode on, exceptions and errors will be outputted.
+* You will also gain the ability to use ?prune=1 GET parameter to
+* delete all the pregenerated thumbnails.
+*/
+const DEBUG_MODE = false;
+
+/**
+* Transforming errors to exceptions
+*/
+function exception_error_handler($severity, $message, $file, $line) {
+    throw new ErrorException($message, 0, $severity, $file, $line);
+}
+
+if (DEBUG_MODE) {
+  set_error_handler("exception_error_handler");
+} else {
+  if ((isset($_GET['prune']) && $_GET['prune'] == 1) || (isset($_GET['skipThumbnailGeneration']) && $_GET['skipThumbnailGeneration'] == 1)) {
+    header("Content-Type: application/json; charset=UTF-8");
+    http_response_code(500);
+    echo json_encode(array("status_code: " => 500, "message" => "Prune and/or skipThumbnailGeneration is set in GET request, but DEBUG_MODE is off. You can not do those without DEBUG_MODE enabled."));
+    exit;
+  }
+}
+
+/**
+* Configure stuff here.
+*/
+const GALLERY_PATH = 'gallery/';
+const RESIZED_STRING_PREFIX = '-rto-'; //can be anything. Just so you can distinguish generated files from originals
+const SUPPORTED_THUMB_SIZES = array('400x250', '250x250');
+const SUPPORTED_MODES = array('fit', 'fitHeight'); //other possibilities: 'fill', 'fitWidth'
+const SUPPORTED_EXTENSIONS = array('jpg', 'jpeg', 'png');
+const SIZE_CACHE_FILE_PATH = GALLERY_PATH . 'size-cache.txt';
+const CACHE_DATA_DELIMITER = '/';
+const CACHE_ITEM_DELIMITER = "\n";
+
+function generateRegexString()
+{
+  $strings = [];
+  foreach (SUPPORTED_THUMB_SIZES as $size) {
+    foreach (SUPPORTED_MODES as $mode) {
+      $strings[] = preg_quote(RESIZED_STRING_PREFIX . $mode . '-' . $size);
+    }
+  }
+
+  return implode('|', $strings);
+}
+
+function isOriginalSize($fileName)
+{
+  $matches = [];
+  preg_match('/^.*(' . generateRegexString() . ')\.(' . implode('|', SUPPORTED_EXTENSIONS) . ')$/i', $fileName, $matches);
+  if (count($matches) > 2) {
+    return false;
+  }
+  return true;
+}
+
+function hasSupportedExtension($fileName)
+{
+  $matches = [];
+  preg_match('/^(.*)\.(' . implode('|', SUPPORTED_EXTENSIONS) . ')$/im', $fileName, $matches);
+  if (sizeof($matches) === 3) {
+    return true;
+  }
+
+  return false;
+}
+
+function getImageDimensions($imageFileName)
+{
+  $image = new Imagick(GALLERY_PATH . $imageFileName);
+  return $image->getImageGeometry();
+}
+
+function resizeToFit($image, $width, $height)
+{
+  return $image->thumbnailImage((int)$width, (int)$height, true);
+}
+
+function resizeToFitHeight($image, $height)
+{
+  return $image->thumbnailImage(0, (int)$height);
+}
+
+function resizeToFitWidth($image, $width)
+{
+  return $image->thumbnailImage((int)$width, 0);
+}
+
+function resizeToFill($image, $width, $height)
+{
+  return $image->cropThumbnailImage((int)$width, (int)$height);
+}
+
+function generateFileName($originalFileName, $modeString, $sizeString)
+{
+  $parsedFilename = [];
+  preg_match('/^(.*)\.(' . implode('|', SUPPORTED_EXTENSIONS) . ')$/i', $originalFileName, $parsedFilename);
+  $filenameUpToExtension = $parsedFilename[1];
+  $extension = $parsedFilename[2];
+
+  return $filenameUpToExtension . RESIZED_STRING_PREFIX . $modeString . '-' . $sizeString . '.' . $extension;
+}
+
+function generateThumbnails($originalFileName)
+{
+  $originalImage = new Imagick(GALLERY_PATH . $originalFileName);
+  foreach (SUPPORTED_THUMB_SIZES as $size) {
+    foreach (SUPPORTED_MODES as $mode) {
+      $thumbnailFileName = GALLERY_PATH . generateFileName($originalFileName, $mode, $size);
+
+      if (file_exists($thumbnailFileName)) {
+        continue;
+      }
+
+      $imageResized = clone $originalImage;
+
+      $width = substr($size, 0, strpos($size, 'x'));
+      $height = substr($size, strpos($size, 'x') + 1);
+
+      if ($mode === 'fit') {
+        resizeToFit($imageResized, $width, $height);
+      } elseif ($mode === 'fitHeight') {
+        resizeToFitHeight($imageResized, $height);
+      } elseif ($mode === 'fitWidth') {
+        resizeToFitWidth($imageResized, $width);
+      } elseif ($mode === 'fill') {
+        resizeToFill($imageResized, $width, $height);
+      }
+
+      $imageResized->writeImage($thumbnailFileName);
+    }
+  }
+}
+
+function getThumbnails($originalFileName)
+{
+  $thumbnails = [];
+  foreach (SUPPORTED_THUMB_SIZES as $size) {
+    foreach (SUPPORTED_MODES as $mode) {
+      $thumbnailFileName = generateFileName($originalFileName, $mode, $size);
+      if (file_exists(GALLERY_PATH . $thumbnailFileName)) {
+        $thumbnails[$mode][$size] = $thumbnailFileName;
+      } else {
+        $thumbnails[$mode][$size] = $originalFileName;
+      }
+    }
+  }
+
+  return $thumbnails;
+}
+
+function getSizesFromCache()
+{
+  $sizes = [];
+  if (file_exists(SIZE_CACHE_FILE_PATH)) {
+    $cache = file_get_contents(SIZE_CACHE_FILE_PATH);
+    $cacheItems = explode(CACHE_ITEM_DELIMITER, $cache);
+
+    foreach ($cacheItems as $item) {
+      if (empty($item)) {
+        continue;
+      }
+
+      $itemArray = explode(CACHE_DATA_DELIMITER, $item);
+      $sizes[$itemArray[0]] = [$itemArray[1],$itemArray[2]];
+    }
+  }
+
+  return $sizes;
+}
+
+function appendSizesToCache($imageIdentifier, $width, $height)
+{
+  $cacheItemString = $imageIdentifier . CACHE_DATA_DELIMITER . $width . CACHE_DATA_DELIMITER . $height . CACHE_ITEM_DELIMITER;
+  file_put_contents(SIZE_CACHE_FILE_PATH, $cacheItemString, FILE_APPEND);
+}
+
+function getCahcedOrCacheImageSize(&$currentCache, $imageFileName)
+{
+  if (!isset($currentCache[$imageFileName])) {
+    $dimensions = getImageDimensions($imageFileName);
+    appendSizesToCache($imageFileName, $dimensions['width'], $dimensions['height']);
+    $currentCache[$imageFileName] = [$dimensions['width'], $dimensions['height']];
+  }
+
+  return $currentCache[$imageFileName];
+}
+
+function flushSizesCache()
+{
+  if (file_exists(SIZE_CACHE_FILE_PATH)) {
+    unlink(SIZE_CACHE_FILE_PATH);
+  }
+}
+
+try {
+  $fileNames = scandir(GALLERY_PATH);
+  $prune = (DEBUG_MODE && (isset($_GET['prune']) && $_GET['prune']) == 1) ? true : false;
+  $skipThumbnailGeneration = (DEBUG_MODE && (isset($_GET['skipThumbnailGeneration']) && $_GET['skipThumbnailGeneration']) == 1) ? true : false;
+
+  $sizesCache = [];
+  $images = [];
+
+  if ($prune) {
+    flushSizesCache();
+
+    foreach ($fileNames as $fileName) {
+      if (!is_dir(GALLERY_PATH . $fileName) && hasSupportedExtension($fileName)) {
+        if (!isOriginalSize($fileName)) {
+          unlink(GALLERY_PATH . $fileName);
+        }
+
+        $fileNames = scandir(GALLERY_PATH);
+      }
+    }
+  }
+
+  $sizesCache = getSizesFromCache();
+  foreach ($fileNames as $fileName) {
+    if (is_dir(GALLERY_PATH . $fileName) || !hasSupportedExtension($fileName)) {
+      continue;
+    }
+
+    if (isOriginalSize($fileName)) {
+      if (!$skipThumbnailGeneration) {
+        generateThumbnails($fileName);
+      }
+
+      $size = getCahcedOrCacheImageSize($sizesCache, $fileName);
+
+      $images[] = array(
+        'original' => $fileName,
+        'width' => $size[0],
+        'height' => $size[1],
+        'thumbnails' => getThumbnails($fileName),
+      );
+
+    }
+  }
+
+  header("Content-Type: application/json; charset=UTF-8");
+  echo json_encode($images);
+} catch (Exception $e) {
+  
+  if (DEBUG_MODE) {
+    throw $e;
+  }
+  
+  header("Content-Type: application/json; charset=UTF-8");
+  http_response_code(500);
+  echo json_encode(array("status_code: " => 500, "message" => "Something got f-... Went wrong."));
+  exit;
+}
